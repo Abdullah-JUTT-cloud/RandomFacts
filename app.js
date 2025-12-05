395db2e48dabfd8cab2dbcba3ef6397303216568
Abdullah <itxabdullah44@gmail.com>
2025-12-05 23:03:02 +0500

    with joke

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let btn=document.querySelector("button");
+let btn=document.querySelector(".f");
 btn.addEventListener("click",async ()=>{
     let fact=await getfacts();
     let p=document.getElementById("result");
@@ -11,5 +11,21 @@ async function getfacts() {
     }catch(err){
         console.log(err);
         return "NO facts found! sorry";
+    }
+}
+let btn2=document.querySelector(".j");
+btn2.addEventListener("click",async ()=>{
+    let joke=await getjokes();
+    let p2=document.getElementById("result2");
+    p2.innerText=joke;
+});
+async function getjokes() {
+    try{
+        let config={headers: {Accept: "application/json"}};
+        let res=await axios.get("https://icanhazdadjoke.com/",config);
+        return res.data.joke;
+    }catch(err){
+        console.log(err);
+        return "No Jokes Found! sorry";
     }
 }
